48e6e8454f8a3a8a56fcff31a5a04cdef8c807e9
krushna <krushnakhodape2000@gmail.com>
2026-06-02 18:26:39 +0530

    added krushna.spec.ts file

---
+++ tests/krushna.spec.ts
@@ -0,0 +1,9 @@
+import {test,expect} from '@playwright/test';
+
+test ("test", async({page})=>{
+
+    await page.goto("https://egazette.gov.in/(S(3eeklm4oag4ogntunb4qjfbv))/default.aspx");
+
+    
+
+})
